f32d2767974012b3a5ef5a40e3073704959c98af
Pratik Shah <psh@narola.email>
2024-01-12 20:08:00 +0530

    Designed Left hand side panel for available documents.

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import RightArrowIcon from "../assets/right-arrow.png";
+
 interface AccordionProps {
   title: string;
   content?: string[];
@@ -7,32 +9,62 @@ interface AccordionProps {
 
 export const Accordion: React.FC<AccordionProps> = ({ title, content }) => {
   return (
-    <div className="py-5">
-      <details className="group">
-        <summary className="flex justify-between items-center font-medium cursor-pointer list-none">
-          <span>{title}</span>
+    <div className="">
+      <details className="group my-5">
+        <summary className="flex justify-between items-center font-medium cursor-pointer list-none ">
+          <span
+            style={{
+              fontSize: "16px",
+              lineHeight: "16px",
+            }}>
+            {title}
+          </span>
+
           <span className="transition group-open:rotate-180">
             <svg
               fill="none"
-              height="24"
+              height="12"
               shape-rendering="geometricPrecision"
               stroke="currentColor"
               stroke-linecap="round"
               stroke-linejoin="round"
               stroke-width="1.5"
               viewBox="0 0 24 24"
-              width="24">
+              width="12">
               <path d="M6 9l6 6 6-6"></path>
             </svg>
           </span>
         </summary>
-        <p className="text-neutral-600 mt-3 group-open:animate-fadeIn">
+
+        <div
+          className=" group-open:animate-fadeIn w-full border"
+          style={{
+            border: "0px, 1px, 0px, 1px solid #E5E7EB",
+          }}>
           {content?.map((content, i) => (
-            <div className="flex flex-col" key={i}>
-              {content}
+            <div
+              className="flex gap-2.5 rounded justify-between p-2"
+              key={i}
+              style={{
+                padding: "8px 6px 8px 6px",
+              }}>
+              <span>{content}</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 25 25"
+                height={13}
+                width={13}>
+                <title>Artboard-34</title>
+                <g id="Right-2" data-name="Right">
+                  <polygon
+                    points="17.5 5.999 16.793 6.706 22.086 11.999 1 11.999 1 12.999 22.086 12.999 16.792 18.294 17.499 19.001 24 12.499 17.5 5.999"
+                    style={{ fill: "#232326" }}
+                  />
+                </g>
+              </svg>
             </div>
           ))}
-        </p>
+        </div>
       </details>
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,24 @@
+import { Accordion } from "../components/Accordion";
 import SearchUI from "../components/SearchUI";
 import { SelectWithSearch } from "../components/SelectWithSearch";
+import ToggleSwitch from "../components/ToggleSwitch";
+import { accordionItems } from "../data/accordion";
 import { filterDropDownMenuItems } from "../data/dropdown";
 
 const Home = () => {
   return (
     <div className="flex h-screen w-screen gap-6 flex-wrap bg-[#D1D5DB] p-4">
-      <div className="bg-white flex-1 border border-solid border-[#D1D5DB] p-4 flex flex-col gap-3 rounded-lg ">
+      <div className="bg-white flex-1  border-[#D1D5DB] p-4 flex flex-col gap-3 rounded-lg ">
         <p className="text-base font-medium text-[#111928]">
           Available Documents
         </p>
-        <div className="border border-solid flex flex-col gap-3 w-full h-full">
+        <div className=" flex flex-col gap-3 w-full h-full">
           <div className="flex flex-col gap-3">
             <SearchUI />
             <p className="text-sm font-medium text-[#111928] text-left">
               Filter by:
             </p>
-            <div className="flex w-full border ">
+            <div className="grid grid-rows-2 grid-flow-col gap-3">
               {filterDropDownMenuItems.map((el, i) => (
                 <div className="">
                   <SelectWithSearch
@@ -26,6 +29,23 @@ const Home = () => {
                 </div>
               ))}
             </div>
+            <div
+              className="flex justify-between"
+              style={{
+                padding: "8px 6px ",
+              }}>
+              <p className="text-sm font-medium text-[#111928] my-auto p">
+                53 Available Documents
+              </p>
+              <ToggleSwitch />
+            </div>
+          </div>
+          <div className="flex-grow ">
+            {accordionItems.map((item, i) => (
+              <div className="flex flex-col">
+                <Accordion title={item?.title} content={item.content} key={i} />
+              </div>
+            ))}
           </div>
         </div>
       </div>
